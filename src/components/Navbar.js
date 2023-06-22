0e0c0f93dc7aea9202530fad8b99117e556b1b5c
Yuvashree <yuvashree.m22122003@gmail.com>
2023-06-22 16:59:49 +0530

    navbar created

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <div className="nav-bar">
+        <h1>Navbar elements</h1>
+        <h4>Home</h4>
+        <h4>About</h4>
+        <h4>Contact</h4>
+        <h4>Services</h4>
+    </div>
+  )
+}
+
+export default Navbar;
