d80d46c8c9392359488c624ca0da055c0cbff8b7
Yuvashree <yuvashree.m22122003@gmail.com>
2023-06-22 17:14:18 +0530

    Navbar styles are added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import "./Navbar.css";
 function Navbar() {
   return (
     <div className="nav-bar">
-        <h1>Navbar elements</h1>
+        <h1>E-Mart</h1>
         <h4>Home</h4>
         <h4>About</h4>
         <h4>Contact</h4>
