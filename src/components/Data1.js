1741c1fa771e52c93dce640bd0b210014c8f07e0
Yuvashree <yuvashree.m22122003@gmail.com>
2023-06-23 14:46:51 +0530

    images are added

--- src/components/Data1.js
+++ src/components/Data1.js
@@ -14,66 +14,58 @@ const Destination = () =>{
 
         <Data
         className="first-des"
-        heading="MANALI-The Paradise"
-        text="With spectacular valleys, breathtaking views, 
-        snowcapped mountains, and lush forests of oak, 
-        deodar, and pine, Manali is a magical hill station 
-        at the northern end of Kullu valley in Himachal 
-        Pradesh. Gifted to the world by the mighty Himalayas, 
-        it is known to be one of the most popular destinations 
-        for Honeymooners. Manali offers magnificent views of 
-        the Pir Panjal and the Dhauladhar ranges, covered 
-        in a thick blanket of snow for most of the year."
+        heading="Fresh Fruits"
+        text= " Velit ut tortor pretium viverra suspendisse 
+        potenti nullam.Volutpat commodo sed egestas egestas 
+        fringilla phasellus faucibus scelerisque. Dignissim 
+        diam quis enim lobortis scelerisque fermentum dui. 
+        Gravida neque convallis a cras semper. In fermentum 
+        posuere urna nec tincidunt praesent semper feugiat."
         img1={i1}
         img2={i1}
         />
 
        <Data
         className="first-des-reverse"
-        heading="COORG-The Scotland of India"
-        text="Coorg, officially known as Kodagu, is the most 
-        affluent hill station in Karnataka. It is well known 
-        for its breathtakingly exotic scenery and lush greenery. 
-        Forest covered hills, spice and coffee plantations 
-        only add to the landscape. Madikeri is the region's 
-        centre point with all transportation for getting around 
-        starting from here. On a visit to Coorg, cover the beautiful 
-        towns like Virajpet, Kushalnagar, Gonikoppal, Pollibetta, 
-        and Somwarpet, and experience the beautiful concept of 
-        homestays to make your experience more memorable! "
+        heading="Fresh Vegetables"
+        text="Aliquet eget sit amet tellus cras adipiscing. 
+        Egestas diam in arcu cursus euismod. Viverra mauris 
+        in aliquam sem fringilla ut.Montes nascetur ridiculus
+        mus mauris vitae ultricies leo integer. Ut placerat 
+        orci nulla pellentesque dignissim enim sit. Sollicit
+        udin nibh sit amet commodo nulla facilisi nullam. 
+        Turpis massa tincidunt dui ut. Vulputate sapien nec 
+        sagittis aliquam malesuada."
         img1={i3}
         img2={i1}
         />
 
         <Data
         className="first-des"
-        heading="MUSSOORIE-The Queen of HillStations"
-        text="Mussoorie is one of the most popular hill stations 
-        located at a distance of 290 km from Delhi in the 
-        Dehradun district of Uttarakhand. With a backdrop of the 
-        Shivalik range of Himalayas and doon valley, Mussoorie, 
-        also known as Queen of The Hills, stands at an altitude of 
-        7000 feet above sea level. With a cool and pleasant 
-        climate throughout the year,Mussoorie was once the 
-        British summer capital. One of the most popular tourist 
-        attractions in Mussoorie is The Mall, which is a slightly 
-        steep boulevard with eateries and other shops lining 
-        its entire stretch."
+        heading="Dairy Products"
+        text="Montes nascetur ridiculus mus mauris vitae 
+        ultricies leo integer. Ut placerat orci nulla pellentes
+        que dignissim enim sit. Sollicitudin nibh sit amet 
+        commodo nulla facilisi nullam. Turpis massa tincidunt 
+        dui ut. Vulputate sapien nec sagittis aliquam malesuada.
+        Felis eget nunc lobortis mattis aliquam faucibus purus. 
+        Nibh nisl condimentum id venenatis a."
         img1={i5}
         img2={i1}
         />
 
         <Data
         className="first-des-reverse"
-        heading="DARJEELING-Queen of Himalayas"
-        text="Darjeeling, the former summer capital of India under 
-        the British Raj, has evolved into one of India's most 
-        sought-after hill stations. This picturesque hill destination 
-        in West Bengal is ideal for a romantic honeymoon. Darjeeling, 
-        nestled among acres of tea estates, is 2,050 metres above sea 
-        level and thus has a cool climate all year.The Darjeeling Himalayan
-        Railway established back in 1881 is conferred the title of 
-        World Heritage Status by UNESCO. "
+        heading="Other Groceries"
+        text=" Id volutpat lacus laoreet non curabitur 
+        gravida. Dolor sed viverra ipsum nunc. Bibendum ut 
+        tristique et egestas quis ipsum. Commodo quis 
+        imperdiet massa tincidunt nunc pulvinar sapien. 
+        Nec ullamcorper sit amet risus nullam eget felis 
+        eget. Quis commodo odio aenean sed adipiscing diam 
+        donec. Non arcu risus quis varius. Mollis aliquam ut 
+        porttitor leo a diam sollicitudin tempor id. Amet 
+        risus nullam eget felis eget nunc. "
         img1={i7}
         img2={i1}
         />
