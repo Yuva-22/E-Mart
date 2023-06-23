bcdd12bafdd33e9a28e784e47d46b4254a9aa271
Yuvashree <yuvashree.m22122003@gmail.com>
2023-06-23 12:23:41 +0530

    Hero image was added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Navbar from "./components/Navbar";
+import img1 from "./images/img1.jpeg";
+
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <img className="hero-img" alt="hero img" src={img1} />
     </div>
   );
 }
